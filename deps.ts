1d5328f26411568013784eabc4aa5dbb0fed37b3
mel9y <me@mel9y.dev>
2023-03-12 23:20:40 +0900

    feat: logger system

--- deps.ts
+++ deps.ts
@@ -1,2 +1,4 @@
 // reference: https://deno.land/manual@v1.30.0/examples/manage_dependencies
 export * from "https://deno.land/x/discordeno@18.0.1/mod.ts"
+export * from "https://deno.land/std@0.179.0/log/logger.ts"
+export * as log from "https://deno.land/std@0.179.0/log/mod.ts"
